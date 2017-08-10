54481f85dccb9c8a09968ddb2ad97f4b3b3e479e
EC2 Default User <ec2-user@ip-10-0-10-153.us-west-2.compute.internal>
2017-08-10 01:15:14 +0000

    Need to specify a region

--- labs/lab-21/validate-stack.sh
+++ labs/lab-21/validate-stack.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 TEMPLATELOCATION=${1:-file://$(pwd)/cloudformation.yml}
+REGION=${2:-us-west-2}
 
-VALIDATE="aws cloudformation validate-template --template-body ${TEMPLATELOCATION}"
+VALIDATE="aws --region ${REGION} cloudformation validate-template --template-body ${TEMPLATELOCATION}"
 echo ${VALIDATE}
 ${VALIDATE}
